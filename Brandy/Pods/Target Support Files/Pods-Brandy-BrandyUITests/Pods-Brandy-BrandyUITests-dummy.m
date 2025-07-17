02c82e00f57c5c52c8ef4fcb4305e522902e7314
hadeer ahmed <hadeerA7mdElmetwally@gmail.com>
2025-07-17 14:25:07 +0300

    add pod

---
+++ Brandy/Pods/Target Support Files/Pods-Brandy-BrandyUITests/Pods-Brandy-BrandyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Brandy_BrandyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Brandy_BrandyUITests
+@end
---
+++ Brandy/Pods/Target Support Files/Pods-BrandyTests/Pods-BrandyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BrandyTests : NSObject
+@end
+@implementation PodsDummy_Pods_BrandyTests
+@end
