68d1194a76b56bacfb20f9d717c42c568c964763
hadeer ahmed <hadeerA7mdElmetwally@gmail.com>
2025-07-18 11:47:11 +0300

    home ui

--- Brandy/Brandy/Scenes/HomeScreen/View/Controller/ViewController.swift
+++ Brandy/Brandy/Scenes/HomeScreen/View/Controller/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var homeCollection: UICollectionView!
+    
+    private var isGridView = false
+    private var toggleButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupToggleButton()
     }
 
 
 }
 
+
+// MARK: -  func extension
+
+extension ViewController{
+    
+    func setupToggleButton() {
+        toggleButton = UIButton(type: .custom)
+        toggleButton.setImage(UIImage(named: "list"), for: .normal)
+        toggleButton.addTarget(self, action: #selector(toggleLayout), for: .touchUpInside)
+        toggleButton.frame = CGRect(x: 0, y: 0, width: 48, height: 48)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: toggleButton)
+
+    }
+    
+    
+    @objc func toggleLayout() {
+        isGridView.toggle()
+//        collectionView.setCollectionViewLayout(createLayout(), animated: true)
+//        collectionView.reloadData()
+
+        let imageName = isGridView ? "grid" : "list"
+        toggleButton.setImage(UIImage(named: imageName), for: .normal)
+    }
+
+}
