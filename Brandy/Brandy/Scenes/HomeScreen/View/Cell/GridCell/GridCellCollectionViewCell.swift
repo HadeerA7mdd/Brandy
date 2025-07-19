7e74b35a0202c7adcdc36536a614c1bc9b3e8389
hadeer ahmed <hadeerA7mdElmetwally@gmail.com>
2025-07-19 22:31:20 +0300

    add home cells

---
+++ Brandy/Brandy/Scenes/HomeScreen/View/Cell/GridCell/GridCellCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  GridCellCollectionViewCell.swift
+//  Brandy
+//
+//  Created by Hader on 19/07/2025.
+//
+
+import UIKit
+
+class GridCellCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Brandy/Brandy/Scenes/HomeScreen/View/Cell/ListCell/ListCellCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ListCellCollectionViewCell.swift
+//  Brandy
+//
+//  Created by Hader on 19/07/2025.
+//
+
+import UIKit
+
+class ListCellCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
