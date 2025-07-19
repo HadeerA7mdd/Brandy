d87a85b7fbc83d8e8a8dc77890f6659cc3313d9a
hadeer ahmed <hadeerA7mdElmetwally@gmail.com>
2025-07-19 22:53:06 +0300

    add list cell ui

--- Brandy/Brandy/Scenes/HomeScreen/View/Cell/GridCell/GridCellCollectionViewCell.swift
+++ Brandy/Brandy/Scenes/HomeScreen/View/Cell/GridCell/GridCellCollectionViewCell.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class GridCellCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var productImg: UIImageView!
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productRate: UILabel!
+    @IBOutlet weak var productReviewCount: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- Brandy/Brandy/Scenes/HomeScreen/View/Cell/ListCell/ListCellCollectionViewCell.swift
+++ Brandy/Brandy/Scenes/HomeScreen/View/Cell/ListCell/ListCellCollectionViewCell.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ListCellCollectionViewCell: UICollectionViewCell {
-
+    @IBOutlet weak var productReviewCount: UILabel!
+    
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var productRate: UILabel!
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productImg: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
