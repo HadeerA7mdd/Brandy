1683f6ccf9cbfb5ec9dbb59c94a449cf42994674
hadeer ahmed <hadeerA7mdElmetwally@gmail.com>
2025-07-20 16:11:21 +0300

    handle image stretching in product details

--- Brandy/Brandy/Scenes/ProductDetailsScreen/View/Controller/ProductDetailsViewController.swift
+++ Brandy/Brandy/Scenes/ProductDetailsScreen/View/Controller/ProductDetailsViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ProductDetailsViewController: UIViewController {
     
+    @IBOutlet weak var imgHeight: NSLayoutConstraint!
     @IBOutlet weak var productDescrp: UILabel!
     @IBOutlet weak var productPrice: UILabel!
     @IBOutlet weak var productReviewCount: UILabel!
@@ -19,9 +20,13 @@ class ProductDetailsViewController: UIViewController {
     @IBOutlet weak var scrollview: UIScrollView!
     
     var productDetails : Product?
+    let maxImageHeight: CGFloat = 440
+     let minImageHeight: CGFloat = 120
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollview.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -32,4 +37,20 @@ class ProductDetailsViewController: UIViewController {
     }
 
 
+}
+
+
+
+extension ProductDetailsViewController :UIScrollViewDelegate{
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset.y
+
+        // Only shrink, never expand
+        let newHeight = max(minImageHeight, maxImageHeight - offset)
+        imgHeight.constant = newHeight
+        UIView.animate(withDuration: 0.1) {
+            self.view.layoutIfNeeded()
+        }
+    }
 }
